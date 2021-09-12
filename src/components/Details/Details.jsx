99f07462ecfdd8ab9b12e9b42dbf9b3c060ae952
Dipan Ghosh <dipan.ghosh@hotmail.com>
2021-09-13 01:47:00 +0530

    created card

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core'
 import { Doughnut } from 'react-chartjs-2'
 
+import useStyles from './styles'
 
 
-const Details = () => {
+const Details = ( {title} ) => {
+    const classes = useStyles();
+
     return (
         <div>
-            <CardHeader title="Income">
-            <CardContent>
-                <Typography variant="h5"> $50 </Typography>
-                <Doughnut data="DATA"> </Doughnut>
-            </CardContent> 
-            </CardHeader>            
+            <Card className={ title == 'Income' ? classes.income: classes.expense}>
+                <CardHeader title={ title }/>
+                <CardContent>
+                    <Typography variant="h5"> $50 </Typography>
+                    {/*<Doughnut data="DATA"> </Doughnut>*/}
+                </CardContent> 
+            </Card>
         </div>
     )
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core'
-import { mergeClasses } from '@material-ui/styles'
+
+import useStyles  from './styles'
 
 const Main = () => {
+    const classes = useStyles() 
+
     return (
-        <Card className={mergeClasses.root}>
+        <Card className={classes.root}>
             <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
-            
+            <CardContent>
+                <Typography align="center" variant="h5"> Total Balance </Typography>
+                <Typography variant="subtitle1" style={{ lineHeight: '1.5rem', marginTop: '20px' }}> 
+
+                </Typography>
+                <Divider />
+            </CardContent>
+
+            <CardContent className={classes.CardContent}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            {/* <List> */}
+                        </Grid>
+                    </Grid>
+            </CardContent>
         </Card>
     )
 }
