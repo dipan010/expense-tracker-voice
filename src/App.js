8e72ef29b8d376382b004f63fe8ba177903a45a9
Dipan Ghosh <dipan.ghosh@hotmail.com>
2021-09-13 01:47:36 +0530

    imported main

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import { Grid } from '@material-ui/core';
 
-import Details from './components/Details/Details';
+import Main from './components/Main/Main'
+import Details from './components/Details/Details'
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+
     return (
         <div>
-            <Grid container spacing={0} alignItems="center" justify="center" style={{ height: '100vh' }}>
+            <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '100vh' }}>
                 <Grid item xs={12} sm={4}>
-                    <Details/>
+                    <Details title="Income"/>
                 </Grid>
                 <Grid item xs={12} sm={4}>
-                    <Details/>
+                    <Main/>
                 </Grid>
                 <Grid item xs={12} sm={4}>
-                    <Details/>
+                    <Details title="Expense"/>
                 </Grid>
             </Grid>
         </div>
