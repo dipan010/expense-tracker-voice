cfd7f28d466954662d95081a71fed60de74511fd
Dipan Ghosh <dipan.ghosh@hotmail.com>
2021-09-14 12:09:11 +0530

    using del_transactions

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { List as MUList, ListItem, ListItemAvatar, ListItemText, Avatar, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core'
 import { Delete, MoneyOff } from '@material-ui/icons'
 
+import { ExpenseTrackerContext } from '../../../context/context'
 import useStyles from './styles'
 
 const List = () => {
     const classes = useStyles()
-  
+    const { deleteTransaction } = useContext(ExpenseTrackerContext);
+
+    console.log(deleteTransaction)
+
     const transactions = [
         {
             id: 1, 
