03aab51021ec2088ac54c1559ce4669efb59572f
Dipan Ghosh <dipan.ghosh@hotmail.com>
2021-09-14 12:14:05 +0530

    minor fix

--- src/context/context.js
+++ src/context/context.js
@@ -7,10 +7,23 @@ const initialState = []
 export const ExpenseTrackerContext = createContext(initialState)
 
 export const Provider = ({ children }) => {
-    const [state, dispatch] = useReducer(contextReducer, initialState, init)
+    const [transactions, dispatch] = useReducer(contextReducer, initialState)
     
+    const deleteTransaction = (id) => {
+        dispatch({ type: 'DELETE_TRANSACTION', payload: id })
+    }
+
+    const addTransaction = (transaction) => {
+        dispatch({ type: 'ADD_TRANSACTION', payload: transaction })
+    }
+
     return (
-        <ExpenseTrackerContext.Provider value={{ appName: 'Expense Tracker' }}>
+        <ExpenseTrackerContext.Provider value={
+            {
+                deleteTransaction,
+                addTransaction 
+            }
+         }>
             { children }
         </ExpenseTrackerContext.Provider>
     )
