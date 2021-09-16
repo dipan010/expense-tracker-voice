64fe639edefc73e2dffdea35608409027a6a54e3
Dipan Ghosh <dipan.ghosh@hotmail.com>
2021-09-16 19:32:25 +0530

    integrated speechly

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { SpeechProvider } from '@speechly/react-client'
 
 import App from './App'
 import { Provider } from './context/context'
 import './index.css'
 
 ReactDOM.render(
-    <Provider>
-        <App />
-    </Provider>,
+    <SpeechProvider appId="cc3bce7e-2d5c-4d45-a534-4ecbd362f42c" language="en-US">
+        <Provider>
+            <App />
+        </Provider>
+    </SpeechProvider>,
     document.getElementById('root')
 );
